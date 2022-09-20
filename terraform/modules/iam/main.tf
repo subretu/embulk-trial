86fa408505099ed3a36aa88d4d6c8c79f750651e
miyahara <50367145+subretu@users.noreply.github.com>
2022-09-21 01:16:35 +0900

    add version

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -23,6 +23,7 @@ data "aws_iam_policy" "systems_manager" {
 }
 
 data "aws_iam_policy_document" "ec2_s3_for_embulk_document" {
+  version = "2012-10-17"
   statement {
     actions = ["s3:PutObject",
       "s3:GetObject",
